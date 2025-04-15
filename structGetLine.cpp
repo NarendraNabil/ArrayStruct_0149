ff89ca31aa6d064ed73367499e0b356636b3dafb
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-04-15 22:15:51 +0700

    menambahkan getline pada struct

---
+++ structGetLine.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main() {
+    //membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukkan NAMA = ";
+    getline (cin , mhs.nama);
+    cout << "Masukkan ALAMAT KOTA = ";
+    cin >> mhs.alamat.kota;
+    cout << "Masukkan ALAMAT DESA ";
+    cin >> mhs.alamat.desa;
+
+    cout << endl;
+    cout << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "NAMA = " << mhs.nama << endl;
+    cout << "ALAMAT DESA = " << mhs.alamat.desa << endl;
+    cout << "ALAMAT KOTA = " << mhs.alamat.kota << endl;
+     
+}
