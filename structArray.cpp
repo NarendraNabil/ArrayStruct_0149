dd347f90086f549a47bcd011ba9c3b735f18ecfb
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-04-15 22:28:10 +0700

    menampilkan semua data ke display dengan perulangan for

--- structArray.cpp
+++ structArray.cpp
@@ -32,5 +32,11 @@ int main() {
     cout << endl;
     cout << "Data Mahasiswa " << endl;
 
-
+    for(int n = 0;n < 2; n++){
+        cout << "Data ke- " << n+1 << endl;    
+        cout << "NIM = " << mhs[n].nim << endl;
+        cout << "NAMA = " << mhs[n].nama << endl;
+        cout << "ALAMAT DESA = " << mhs[n].alamat.desa << endl;
+        cout << "ALAMAT KOTA = " << mhs[n].alamat.kota << endl;
+}
 }
